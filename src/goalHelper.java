1fa3c3ee222af814a531300618eb61b76ff514f9
DareusAshield <87916475+DareusAshield@users.noreply.github.com>
2021-07-24 16:36:50 -0700

    Update goalHelper.java

--- src/goalHelper.java
+++ src/goalHelper.java
@@ -1,16 +1,12 @@
 public class goalHelper {
     public static void main(String[] args) {
-        String[] myGoals = {"trim method on ContactDeploy,23", "add a txt to AbstractFRDeploy", "github", "star", "jetbrains academy", "java certification", "Cloud certification", "kotlin certification/google", "python/ml", "cyber-security/cloud", "to-do list"};
+        String[] myGoals = {"", "", "", ""};
 
         for (Object goals : myGoals) {
             System.out.println("One goal of mine is:\n" + goals + "\n");
         }
 
-////        for(int g = 0; g < myGoals; g++){
-////            System.out.println("I have a goal of " + g );
-//
-//
-//        }
+
 
     }
 }
